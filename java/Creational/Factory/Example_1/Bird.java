ce24517cb887a26463b909e2b30cd3dff6d47521
MeqdadDev <meqdad.darweesh@gmail.com>
2024-02-06 22:27:22 +0200

    Factory: re-arrange files of mario game

--- java/Creational/Factory/Example_1/Bird.java
+++
@@ -1,9 +0,0 @@
-package DesignPatterns.Creational.Factory;
-
-public class Bird extends Enemy{
-    public Bird(){
-        setName("Bird");
-        setDamage(20);
-        setHealth(40);
-    }
-}
--- java/Creational/Factory/Example_1/Turtle.java
+++
@@ -1,9 +0,0 @@
-package DesignPatterns.Creational.Factory;
-
-public class Turtle extends Enemy{
-    public Turtle(){
-        setName("Turtle");
-        setDamage(10);
-        setHealth(30);
-    }
-}
